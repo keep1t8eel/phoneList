9f805a056310895008972c34fe0fc9de955743ea
keep1t8eel <showaikh@protonmail.com>
2022-03-10 22:59:40 +0300

    better reusability

--- phonebook.py
+++ phonebook.py
@@ -7,18 +7,19 @@ print("""
 |_|\_\___|\___| .__/|_|\__\___/ \___|\___|_|
               |_|                           
 """)
-def start():
-    file = input("filename: ")
-    file = open(file, "w+")
-    ext = input("extension: ")
-    digits = input("digits: ")
-    added = "1" + digits
-    converted = int(added)
-    for i in range(converted):
-        i = f'{i:0{len(digits)}}\n'
-        i = str (i)
-        file.write (ext + i)
-start()
+
+class phone:
+        file = input("filename:")
+        file = open(file, "w+")
+        ext = input("extension:")
+        digits = input("digits:")
+        added = "1" + digits
+        converted = int(added)
+        for i in range(converted):
+            i = f'{i:0{len(digits)}}\n'
+            i = str (i)
+            file.write (ext + i)
+
 
 
 
